08e684eb22a2afd153cc3e3d58182ce30e848dbf
Juan Cuenca <juan99.cuenca@gmail.com>
2025-01-23 10:59:24 -0300

    responde:: with 200

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::io;
+use std::io::{self, Write};
 use std::net::{TcpListener, TcpStream};
+use std::fs;
 
-fn handle_client(stream: TcpStream) {
-    todo!();
+fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("home.html")?;
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes())?;
+
+    Ok(())
 }
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
     
     for stream in listener.incoming() {
-        handle_client(stream?);
+        handle_client(stream?)?;
     }
 
     Ok(())
