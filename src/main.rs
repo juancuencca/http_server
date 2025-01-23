aeb5a34b44f8ba106943c01644a4b728077bf063
Juan Cuenca <juan99.cuenca@gmail.com>
2025-01-23 10:18:36 -0300

    bind: to a port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    todo!();
+}
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
 }
