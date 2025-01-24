58027368f16f9ab239429510ecaa34d4a0938032
Juan Cuenca <juan99.cuenca@gmail.com>
2025-01-24 16:31:23 -0300

    read: url and respond with file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use std::io::{self, Write};
+use std::io::{self, Write, BufReader, BufRead};
 use std::net::{TcpListener, TcpStream};
 use std::fs;
 
 fn handle_client(mut stream: TcpStream) -> io::Result<()> {
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("home.html")?;
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "home.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    
+    let contents = fs::read_to_string(filename)?;
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
